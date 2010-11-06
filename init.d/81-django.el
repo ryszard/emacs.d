7df335f569b7516f37f4a557c041acce29700a16
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-11-06 23:26:27 +0100

    Added django stuff.

---
+++ init.d/81-django.el
@@ -0,0 +1,18 @@
+;; run Django shell when editing Django Python code
+
+(defun get-file-in-upstream-dir (location filename)
+  (let* ((dir (file-name-directory location))
+         (path (concat dir filename)))
+    (if (file-exists-p path)
+        path
+      (if (not (equal dir "/"))
+        (get-file-in-upstream-dir (expand-file-name (concat dir "../")) filename)))))
+
+(defadvice run-python (before possibly-setup-django-project-environment)
+  (let* ((settings-py (get-file-in-upstream-dir buffer-file-name "settings.py"))
+         (project-dir (file-name-directory settings-py))
+	 (local-site-python (get-file-in-upstream-dir buffer-file-name "site-python")))
+    (if settings-py
+        (progn
+          (setenv "DJANGO_SETTINGS_MODULE" "settings")
+          (setenv "PYTHONPATH" (format "%s:%s" local-site-python project-dir))))))
