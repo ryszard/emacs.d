7082a8228563b6854060b1c5f00a502c824eca33
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-11-07 19:55:25 +0100

    Better keybindings for paredit.

--- init.d/81-paredit-mode.el
+++ init.d/81-paredit-mode.el
@@ -3,8 +3,10 @@
 (add-hook 'clojure-mode-hook 'paredit-mode)
 (let ((paredit-forward-backward (lambda ()
 				  (progn
-				    (local-set-key [s-right] 'paredit-forward)
-				    (local-set-key [s-left] 'paredit-backward)))))
+				    (local-set-key [s-right] 'paredit-forward-slurp-sexp)
+				    (local-set-key [s-left] 'paredit-forward-barf-sexp)
+				    (local-set-key [m-s-right] 'paredit-forward)
+				    (local-set-key [m-s-left] 'paredit-backward)))))
   (add-hook 'emacs-lisp-mode-hook paredit-forward-backward))
 
 
