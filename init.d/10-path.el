b5b484b972f7bb6eca3db159b77adf6b42e72263
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-11-07 19:55:07 +0100

    Add PYTHONPATH.

--- init.d/10-path.el
+++ init.d/10-path.el
@@ -1,5 +1,25 @@
+(require 'cl)
+
+(defun augment-path-like (var &rest paths)
+  "Add `paths` to PATH-like environmental variable."
+  (setenv var
+	  (reduce (lambda (el ac)
+		    (concat ac path-separator el))
+		  (reverse paths)
+		  :initial-value (getenv "PATH"))))
+
+(defun augment-path (&rest paths)
+  (apply 'augment-path-like (cons "PATH" paths)))
+
+
 (when (equal system-type 'darwin)
-  (setenv "PATH" (concat "/opt/local/bin:/usr/local/bin:" (getenv "PATH")))
-  (push "/opt/local/bin" exec-path))
-(setenv "PATH" (concat "/usr/local/bin" path-separator (getenv "PATH")))
+ (augment-path
+  "/opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin"
+  "/opt/local/bin"
+  "/usr/local/bin")
+(augment-path-like 
+ "PYTHONPATH"
+ "/opt/local/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages")
+ (push "/opt/local/bin" exec-path))
+
 
