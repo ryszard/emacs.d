762bec8410d2e8e869491c7749d98b783c472c23
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-11-07 04:23:12 +0100

    Fix bug in django stuff.

--- init.d/81-django.el
+++ init.d/81-django.el
@@ -12,7 +12,9 @@
   (let* ((settings-py (get-file-in-upstream-dir buffer-file-name "settings.py"))
          (project-dir (file-name-directory settings-py))
 	 (local-site-python (get-file-in-upstream-dir buffer-file-name "site-python")))
-    (if settings-py
+    ;; TODO: do the right thing when ther's only `project-dir` or
+    ;; `local-site-python` available.
+    (when (and local-site-python settings-py)
         (progn
           (setenv "DJANGO_SETTINGS_MODULE" "settings")
           (setenv "PYTHONPATH" (format "%s:%s" local-site-python project-dir))))))
