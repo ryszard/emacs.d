6293659ba817fc9152a88e2267b9804b11b2d7ba
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-11-07 03:05:59 +0100

    Move custom-set-variables out of `10-emacs.el`, put .emacs into the repo.

---
+++ dot-emacs.el
@@ -0,0 +1,22 @@
+;; create a symlink from ~/.emacs to this file.
+
+(load "~/.emacs.d/init.el")
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(mouse-wheel-follow-mouse t)
+ '(mouse-wheel-mode t)
+ '(ns-pop-up-frames nil)
+ '(paren-match-face (quote paren-face-match))
+ '(paren-sexp-mode t)
+ '(show-paren-mode t)
+)
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
--- init.d/10-emacs.el
+++ init.d/10-emacs.el
@@ -36,16 +36,3 @@
 (setq-default save-place t)
 (setq save-place-file "~/.emacs.d/.saveplace")
 (require 'saveplace)
-
-(custom-set-variables
-  ;; custom-set-variables was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(column-number-mode t)
- '(mouse-wheel-follow-mouse t)
- '(mouse-wheel-mode t)
- '(ns-pop-up-frames nil)
- '(paren-match-face (quote paren-face-match))
- '(paren-sexp-mode t)
- '(show-paren-mode t))
