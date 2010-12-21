30985f232b4de908c899b7efc7aa39f7a8ce39c0
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-12-21 17:03:57 +0100

    New settings.

--- dot-emacs.el
+++ dot-emacs.el
@@ -6,8 +6,21 @@
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
+ '(coffee-js-mode (quote js-mode))
  '(coffee-tab-width 2)
  '(column-number-mode t)
+ '(hippie-expand-try-functions-list (quote (complete-tag try-complete-file-name-partially try-complete-file-name try-expand-all-abbrevs try-expand-list try-expand-line try-expand-dabbrev try-expand-dabbrev-all-buffers try-expand-dabbrev-from-kill)))
+ '(history-delete-duplicates t)
+ '(history-length 50)
+ '(js-indent-level 2)
+ '(js2-auto-indent-p nil)
+ '(js2-bounce-indent-p nil)
+ '(js2-enter-indents-newline t)
+ '(js2-indent-on-enter-key t)
+ '(js2-missing-semi-one-line-override t)
+ '(js2-mode-indent-ignore-first-tab t)
+ '(js2-strict-missing-semi-warning t)
+ '(minibuffer-depth-indicate-mode t)
  '(mouse-wheel-follow-mouse t)
  '(mouse-wheel-mode t)
  '(ns-pop-up-frames nil)
