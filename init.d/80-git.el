a4a998ba9214278db46b8a5d4772850a206620d2
Ryszard Szopa <ryszard.szopa@gmail.com>
2010-12-21 17:03:00 +0100

    Added git-review.

--- init.d/80-git.el
+++ init.d/80-git.el
@@ -1,1 +1,11 @@
 (require 'git)
+
+(defun git-review (task-number)
+  (interactive "nTask number: ")
+  (let* ((buffer (get-buffer-create "*Review*"))
+         (dir (git-get-top-dir ".")))
+    (switch-to-buffer buffer)
+    (cd dir)
+    (shell-command (format "git log -p --grep='#%d' --reverse" task-number)
+                   buffer buffer)
+    (diff-mode)))
